607bb58044cf422f7d66d5b8976717903ffdaad0
aimee daniells <aimeedaniells@googlemail.com>
2011-12-31 17:04:43 +0000

    Brand new Rails 3.1.3 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fiveyearcalendar::Application.config.secret_token = '53c734bcd23ff2529d0d65ce03aa8b6d2a2fc9be7058bf9cec322d2aed246f395d76caaf846c5c6dd619f85544627a641b50b8f8c8cf5333cc30de680f23b23c'
